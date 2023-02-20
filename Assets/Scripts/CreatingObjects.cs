e6644a52354438432e0d8311df0927593e1e1f3f
s4203312 <s4203312@glos.ac.uk>
2023-02-20 11:54:45 +0000

    ?

--- Assets/Scripts/CreatingObjects.cs
+++ Assets/Scripts/CreatingObjects.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CreatingObjects : MonoBehaviour
 {
+    //Universe
+    [SerializeField] private CanvasGroup panel;
+    [SerializeField] private TMP_InputField sizeUniverseInput;
+    [SerializeField] private TMP_InputField numberOfStarsInput;
+    [SerializeField] private TMP_InputField planetsPerStarInput;
+    public float sizeUniverse;
+    public int numberOfStars;
+    public int planetsPerStar;
+    
     //Stars
     [SerializeField] public GameObject star;
     [SerializeField] public GameObject[] starArray;
@@ -18,7 +28,13 @@ public class CreatingObjects : MonoBehaviour
     //Orbiting
     [SerializeField] public float orbitSpeed;
     private GameObject planetSetting;
-    Vector3[] rotations = new Vector3[] { Vector3.up, Vector3.left, Vector3.right, Vector3.down };
+    Vector3[] rotations = new Vector3[] 
+    { 
+        new Vector3(1,0,0), new Vector3(0, 1, 0), new Vector3(0, 0, 1),
+        new Vector3(1,1,0), new Vector3(0, 1, 1), new Vector3(1, 0, 1),new Vector3(1, 1, 1),
+        new Vector3(-1,0,0), new Vector3(0, -1, 0), new Vector3(0, 0, -1),
+        new Vector3(-1,-1,0), new Vector3(0, -1, -1), new Vector3(-1, 0, -1),new Vector3(-1, -1, -1),
+    };
     [SerializeField] public List<Vector3> rotationDirection;
     private int rotationCounter = 0;
 
@@ -27,6 +43,10 @@ public class CreatingObjects : MonoBehaviour
 
     void Start()
     {
+        sizeUniverse = float.Parse(sizeUniverseInput.text);
+        numberOfStars = int.Parse(numberOfStarsInput.text);
+        planetsPerStar = int.Parse(planetsPerStarInput.text);
+
         Creation();
         Rendering();
     }
@@ -38,18 +58,18 @@ public class CreatingObjects : MonoBehaviour
 
     private void Creation()
     {
-        starArray = new GameObject[10];
-        planetArray = new GameObject[30];
+        starArray = new GameObject[numberOfStars];
+        planetArray = new GameObject[planetsPerStar * numberOfStars];
         rotationDirection = new List<Vector3>();
 
-        for (var i = 0; i < 10; i++)
+        for (var i = 0; i < numberOfStars; i++)
         {
-            var positionStar = new Vector3(Random.Range(-1000.0f, 1000.0f), Random.Range(-1000.0f, 1000.0f), Random.Range(-1000.0f, 1000.0f));
+            var positionStar = new Vector3(Random.Range(-sizeUniverse, sizeUniverse), Random.Range(-sizeUniverse, sizeUniverse), Random.Range(-sizeUniverse, sizeUniverse));
             starArray[i] = Instantiate(star, positionStar, Quaternion.identity);
             starArray[i].transform.tag = "Star";
             starArray[i].transform.name = starNames[Random.Range(0, starNames.Length)] + " " + i;
 
-            for (var j = 0; j < 3; j++)
+            for (var j = 0; j < planetsPerStar; j++)
             {
                 var positionPlanet = new Vector3(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
                 planetArray[planetCounter] = Instantiate(planet, positionPlanet + positionStar, Quaternion.identity, starArray[i].transform);
@@ -73,7 +93,7 @@ public class CreatingObjects : MonoBehaviour
     private void Orbiting() 
     {
         foreach (var star in starArray) {
-            for(var i = 0; i < 3; i++) {
+            for(var i = 0; i < planetsPerStar; i++) {
                 planetSetting = star.gameObject.transform.GetChild(i).gameObject;
                 Vector3 pos = star.transform.localPosition;
                 planetSetting.transform.RotateAround(pos, rotationDirection[rotationCounter], Random.Range(5.0f,30.0f) * Time.deltaTime);
