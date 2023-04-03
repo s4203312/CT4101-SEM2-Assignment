de2229aa44d20715e50383ad32cff626e4538717
s4203312 <s4203312@glos.ac.uk>
2023-04-03 12:28:42 +0100

    teleport anim working

--- Assets/Scripts/SetUp.cs
+++ Assets/Scripts/SetUp.cs
@@ -12,15 +12,32 @@ public class SetUp : MonoBehaviour
     [SerializeField] private TMP_InputField numberOfStarsInput;
     [SerializeField] private TMP_InputField planetsPerStarInput;
 
+    [SerializeField] private GameObject teleport;
+
     public static float sizeUniverse;
     public static int numberOfStars;
     public static int planetsPerStar;
+
+    int timer = 0;
+    bool pressed = false;
 
     public void StartGame() {
         sizeUniverse = float.Parse(sizeUniverseInput.text);
         numberOfStars = int.Parse(numberOfStarsInput.text);
         planetsPerStar = int.Parse(planetsPerStarInput.text);
 
-        SceneManager.LoadScene("StarMap");
+        teleport.SetActive(true);
+        teleport.GetComponent<Animator>().SetTrigger("teleport");
+        pressed = true;
+        
+    }
+    public void Update() {
+        if (pressed) {
+            timer++;
+        }
+        if(timer == 1000) {
+            pressed = false;
+            SceneManager.LoadScene("StarMap");
+        }
     }
 }
