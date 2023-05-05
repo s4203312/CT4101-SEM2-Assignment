b422062b6dbdc8b0c24e04c8605d1f5dc6bd9c1a
Alec Robinson <s4203312@glos.ac.uk>
2023-05-05 11:04:26 +0100

    final

--- Assets/Scripts/SetUp.cs
+++ Assets/Scripts/SetUp.cs
@@ -19,43 +19,47 @@ public class SetUp : MonoBehaviour
 
     //Variables for the teleportation image
     [SerializeField] private GameObject teleport;
-    int timer = 0;
+    float time = 0;
     bool pressed = false;
 
-    public void StartGame() {
+    public void StartGame()
+    {
         //Setting the player inputs as static variable for the map
         //Size of universe switch statement
-        switch(sizeUniverseInput.value) {
+        switch (sizeUniverseInput.value)
+        {
             case 1:
                 sizeUniverse = 500;
                 //Switch case for the amount of stars depending on the size of universe
-                switch (numberOfStarsInput.value) {
+                switch (numberOfStarsInput.value)
+                {
                     case 1:
-                        numberOfStars = 50;
+                        numberOfStars = 20;
                         break;
                     case 2:
-                        numberOfStars = 100;
+                        numberOfStars = 50;
                         break;
                     case 3:
-                        numberOfStars = 150;
+                        numberOfStars = 100;
                         break;
                     default:
                         break;
                 }
-                
+
                 break;
             case 2:
                 sizeUniverse = 1000;
                 //Switch case for the amount of stars depending on the size of universe
-                switch (numberOfStarsInput.value) {
+                switch (numberOfStarsInput.value)
+                {
                     case 1:
-                        numberOfStars = 100;
+                        numberOfStars = 70;
                         break;
                     case 2:
-                        numberOfStars = 150;
+                        numberOfStars = 100;
                         break;
                     case 3:
-                        numberOfStars = 200;
+                        numberOfStars = 150;
                         break;
                     default:
                         break;
@@ -65,15 +69,16 @@ public class SetUp : MonoBehaviour
             case 3:
                 sizeUniverse = 2000;
                 //Switch case for the amount of stars depending on the size of universe
-                switch (numberOfStarsInput.value) {
+                switch (numberOfStarsInput.value)
+                {
                     case 1:
-                        numberOfStars = 150;
+                        numberOfStars = 120;
                         break;
                     case 2:
-                        numberOfStars = 200;
+                        numberOfStars = 150;
                         break;
                     case 3:
-                        numberOfStars = 250;
+                        numberOfStars = 200;
                         break;
                     default:
                         break;
@@ -106,14 +111,28 @@ public class SetUp : MonoBehaviour
         teleport.GetComponent<Animator>().SetTrigger("teleport");
         GetComponent<AudioSource>().Play();                         //Playing a teleporting sound
         pressed = true;
-        
     }
-    public void Update() {
-        if (pressed) {
-            timer++;
+
+    private void Update()
+    {
+        if (pressed)
+        {
+            Teleport();
         }
-        //Loading the level scene after update has been called 1000 times
-        if(timer == 1000) {
+    }
+
+    void Teleport()
+    {
+        Debug.Log(time);
+        while (time < 5)
+        {
+            time += Time.deltaTime;
+            Debug.Log(time);
+            break;
+        }
+        //Loading the level scene after 5 seconds
+        if (time >= 5)
+        {
             pressed = false;
             SceneManager.LoadScene("StarMap");
         }
